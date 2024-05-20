211d1a2b55d45dbac4310e9f5092398d522d52a5
Jaro Habiger <jarohabiger@googlemail.com>
2024-05-20 13:39:01 +0200

    optimize fridge sound

--- game/places/fridge.ts
+++ game/places/fridge.ts
@@ -4,7 +4,7 @@ place.get("door_handle").onClick(() => {
     game.getSound("fridge").pause();
 })
 
-game.getSound("fridge").play();
+game.getSound("fridge").setVolume(0.1).play();
 
 place.getMany(/slot_\d\d/).map(slot => {
     slot.onOtherDrop(item => item.anchor(slot))
