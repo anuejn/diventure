76e9c7de2abc9f0599a4e11c23bf72fde634a8d9
Jaro Habiger <jarohabiger@googlemail.com>
2024-05-21 01:53:14 +0200

    map: go to supermarket backdoor when has sticker

--- game/places/map.ts
+++ game/places/map.ts
@@ -13,13 +13,14 @@ place.get('locksmith').onClick(() => {
     void game.getSound("door_bell2").play();
 })
 
-place.get('supermarket').onClick(() => {
-    game.navigate('supermarket')
-    void game.getSound("door_bell1").play();
-})
 
-place.get('supermarket').onOtherDrop(item => {
-    if (item.itemName == "meme") {
+const itemsInInventory = await game.controls['inventory'].get('backpack_with_inventory').anchoredItemsRecursive();
+place.get('supermarket').onClick(async () => {
+    if (itemsInInventory.findIndex(item => item.itemName == "meme") == -1) {
+        game.navigate('supermarket')
+        void game.getSound("door_bell1").play();
+    } else {
         game.navigate('backdoor_supermarket')
     }
+
 })
