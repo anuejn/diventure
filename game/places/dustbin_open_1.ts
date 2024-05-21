bd26e6dd8f971bbdc876182de051c7946493cd94
lisb-org <lisger.lg@gmail.com>
2024-05-21 17:36:09 +0200

    loads of trash

--- game/places/dustbin_open_1.ts
+++ game/places/dustbin_open_1.ts
@@ -20,4 +20,29 @@ place.get("lid").onClick(() => {
     game.navigate("trashbins");
 })
 
+//there are 27 slots:
+
+await game.spawnItemOnce("futzerl1", place.get("slot_01"))
+await game.spawnItemOnce("can", place.get("slot_02"))
+await game.spawnItemOnce("boxtrash", place.get("slot_03"))
+await game.spawnItemOnce("appletrash", place.get("slot_04"))
+await game.spawnItemOnce("trashbag2", place.get("slot_05"))
+await game.spawnItemOnce("can2", place.get("slot_06"))
+await game.spawnItemOnce("futzerl2", place.get("slot_07"))
+await game.spawnItemOnce("trashbag1", place.get("slot_08"))
+await game.spawnItemOnce("kanister", place.get("slot_09"))
+await game.spawnItemOnce("littlepaper", place.get("slot_10"))
+await game.spawnItemOnce("milktrash", place.get("slot_11"))
+await game.spawnItemOnce("mirror", place.get("slot_12"))
+await game.spawnItemOnce("paperbag", place.get("slot_13"))
+await game.spawnItemOnce("papertrash", place.get("slot_14"))
+await game.spawnItemOnce("paste", place.get("slot_15"))
+await game.spawnItemOnce("trashbag3", place.get("slot_16"))
+await game.spawnItemOnce("plasticbag", place.get("slot_17"))
+await game.spawnItemOnce("toiletpaper", place.get("slot_18"))
+await game.spawnItemOnce("bottletrash", place.get("slot_19"))
 await game.spawnItemOnce("banana", place.get("slot_20"))
+await game.spawnItemOnce("appletrash", place.get("slot_21"))
+await game.spawnItemOnce("trashbag4", place.get("slot_22"))
+await game.spawnItemOnce("trashbag5", place.get("slot_26"))
+
--- game/places/dustbin_open_2.ts
+++ game/places/dustbin_open_2.ts
@@ -19,3 +19,14 @@ place.get("lid").onClick(() => {
     void game.getSound("container").play();
     game.navigate("trashbins");
 })
+
+//mäh
+
+await game.spawnItemOnce("boxtrash", place.get("slot_03"))
+await game.spawnItemOnce("trashbag2", place.get("slot_05"))
+await game.spawnItemOnce("trashbag1", place.get("slot_08"))
+await game.spawnItemOnce("papertrash", place.get("slot_14"))
+await game.spawnItemOnce("trashbag3", place.get("slot_16"))
+await game.spawnItemOnce("plasticbag", place.get("slot_17"))
+await game.spawnItemOnce("trashbag4", place.get("slot_22"))
+await game.spawnItemOnce("trashbag5", place.get("slot_26"))
