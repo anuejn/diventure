d841335d46d61d7d060887b3442adb0669ecb62d
Jaro Habiger <jarohabiger@googlemail.com>
2024-05-26 12:00:38 +0200

    frontdoor_supermarket: make corner not clickable

--- game/places/frontdoor_supermarket.ts
+++ game/places/frontdoor_supermarket.ts
@@ -5,9 +5,8 @@ place.get('entrence').onClick(() => {
 
 
 const itemsInInventory = await game.controls['inventory'].get('backpack_with_inventory').anchoredItemsRecursive();
-place.get('backdoor').onClick(async () => {
-    if (itemsInInventory.findIndex(item => item.itemName == "meme") != -1) {
-        game.navigate('backdoor_supermarket') // doesn't work ... or actually always works and shouldn't !!!
-    }
-})
-
+if (itemsInInventory.findIndex(item => item.itemName == "meme") != -1) {
+    place.get('backdoor').onClick(async () => {
+        game.navigate('backdoor_supermarket')
+    })
+}
