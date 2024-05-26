f7738a3e7e18478940121363514210da3f58589b
Jaro Habiger <jarohabiger@googlemail.com>
2024-05-26 19:21:50 +0200

    randomly generate trashbin contents

--- game/places/dustbin_open_1.ts
+++ game/places/dustbin_open_1.ts
@@ -2,42 +2,37 @@ place.getMany(/slot_\d\d/).map(slot => {
     slot.onOtherDrop(item => item.anchor(slot))
 })
 
-place.get("lid").onClick(() => {
-    void game.getSound("container").play();
-    game.navigate("trashbins");
-})
+const food_list = ["apple", "banana", "bellpeper", "brezel", "butter", "candy", "chocolate", "eggs", "flour", "honey", "juice", "oat_milk", "oil_olive", "oil_sunflower", "sugar", "yogurt"]
+const trash_list = ["appletrash", "bottletrash", "boxtrash", "can", "can2", "futzerl1", "futzerl2", "kanister", "littlepaper", "mirror", "paperbag", "papertrash", "paste", "plasticbag", "toiletpaper"]
+const trashbags = ["trashbag1", "trashbag2", "trashbag3", "trashbag4", "trashbag5"]
 
-//var food_list = ["apple", "banana", "bellpeper", "brezel", "butter", "candy", "chocolate", "eggs", "flour", "honey", "juice", "oat_milk", "oil_olive", "oil_sunflower", "sugar", "yogurt"]
-//var trash_list = ["appletrash", "bottletrash", "boxtrash", "can", "can2", "futzerl2", "futzerl2", "kanister", "littlepaper", "mirror", "paperbag", "papertrash", "paste", "plasticbag", "toiletpaper", "trashbag1", "trashbag2", "trashbag3", "trashbag4", "trashbag5"]
+const slots = []  // there are 27 slots:
+for (let i = 1; i <= 27; i++) {
+    slots.push(place.get(`slot_${i.toString().padStart(2, "0")}`))
+}
 
-//there are 27 slots:
+const golden_ratio = 1.618033988749894;
+const state: Record<string, number> = {};
+function blue_choice<T>(list: T[]): T {
+    const key = JSON.stringify(list);
+    if (!state[key]) state[key] = Math.random();
+    state[key] = (state[key] + golden_ratio) % 1.0;
+    const idx = Math.round(state[key] * (list.length - 1));
+    return list[idx]
+}
+
 if (!place.state.spawnedTrash) {
     place.state.spawnedTrash = true;
-    await game.spawnItemUnique("futzerl1", place.get("slot_01"))
-    await game.spawnItemUnique("can", place.get("slot_02"))
-    await game.spawnItemUnique("boxtrash", place.get("slot_03"))
-    await game.spawnItemUnique("appletrash", place.get("slot_04"))
-    await game.spawnItemUnique("trashbag2", place.get("slot_05"))
-    await game.spawnItemUnique("can2", place.get("slot_06"))
-    await game.spawnItemUnique("futzerl2", place.get("slot_07"))
-    await game.spawnItemUnique("trashbag1", place.get("slot_08"))
-    await game.spawnItemUnique("kanister", place.get("slot_09"))
-    await game.spawnItemUnique("littlepaper", place.get("slot_10"))
-    await game.spawnItemUnique("honey", place.get("slot_11"))
-    await game.spawnItemUnique("mirror", place.get("slot_12"))
-    await game.spawnItemUnique("paperbag", place.get("slot_13"))
-    await game.spawnItemUnique("papertrash", place.get("slot_14"))
-    await game.spawnItemUnique("paste", place.get("slot_15"))
-    await game.spawnItemUnique("trashbag3", place.get("slot_16"))
-    await game.spawnItemUnique("plasticbag", place.get("slot_17"))
-    await game.spawnItemUnique("toiletpaper", place.get("slot_18"))
-    await game.spawnItemUnique("bottletrash", place.get("slot_19"))
-    await game.spawnItemUnique("banana", place.get("slot_20"))
-    await game.spawnItemUnique("appletrash", place.get("slot_21"))
-    await game.spawnItemUnique("trashbag4", place.get("slot_22"))
-    await game.spawnItemUnique("apple", place.get("slot_23"))
-    await game.spawnItemUnique("bellpeper", place.get("slot_24"))
-    await game.spawnItemUnique("brezel", place.get("slot_25"))
-    await game.spawnItemUnique("trashbag5", place.get("slot_26"))
-    await game.spawnItemUnique("candy", place.get("slot_27"))
+
+    await game.spawnItemUnique("flour", blue_choice(slots))
+    await game.spawnItemUnique("sugar", blue_choice(slots))
+    for (let i = 0; i < 15; i++) {
+        await game.spawnItemUnique(blue_choice(food_list), blue_choice(slots))
+    }
+    for (let i = 0; i < 12; i++) {
+        await game.spawnItemUnique(blue_choice(trash_list), blue_choice(slots))
+    }
+    for (let i = 0; i < 8; i++) {
+        await game.spawnItemUnique(blue_choice(trashbags), blue_choice(slots))
+    }
 }
--- game/places/dustbin_open_2.ts
+++ game/places/dustbin_open_2.ts
@@ -1,34 +1,38 @@
-//game.spawnItemOnce("banana", place.get("slot1"))
-
-
 place.getMany(/slot_\d\d/).map(slot => {
     slot.onOtherDrop(item => item.anchor(slot))
 })
 
-/*
-place.getMany(/slot\d/).map(slot => {
-    slot
-        .hide()
-        .onOtherDragStart(() => slot.show())
-        .onOtherDragEnd(() => slot.hide())
-        .onOtherDrop(item => item.anchor(slot))
-})
-*/
+const food_list = ["apple", "banana", "bellpeper", "brezel", "butter", "candy", "chocolate", "eggs", "flour", "honey", "juice", "oat_milk", "oil_olive", "oil_sunflower", "sugar", "yogurt"]
+const trash_list = ["appletrash", "bottletrash", "boxtrash", "can", "can2", "futzerl1", "futzerl2", "kanister", "littlepaper", "mirror", "paperbag", "papertrash", "paste", "plasticbag", "toiletpaper"]
+const trashbags = ["trashbag1", "trashbag2", "trashbag3", "trashbag4", "trashbag5"]
 
-place.get("lid").onClick(() => {
-    void game.getSound("container").play();
-    game.navigate("trashbins");
-})
+const slots = []  // there are 27 slots:
+for (let i = 1; i <= 27; i++) {
+    slots.push(place.get(`slot_${i.toString().padStart(2, "0")}`))
+}
+
+const golden_ratio = 1.618033988749894;
+const state: Record<string, number> = {};
+function blue_choice<T>(list: T[]): T {
+    const key = JSON.stringify(list);
+    if (!state[key]) state[key] = Math.random();
+    state[key] = (state[key] + golden_ratio) % 1.0;
+    const idx = Math.round(state[key] * (list.length - 1));
+    return list[idx]
+}
 
 if (!place.state.spawnedTrash) {
     place.state.spawnedTrash = true;
-    await game.spawnItemUnique("boxtrash", place.get("slot_03"))
-    await game.spawnItemUnique("trashbag2", place.get("slot_05"))
-    await game.spawnItemUnique("trashbag1", place.get("slot_08"))
-    await game.spawnItemUnique("papertrash", place.get("slot_14"))
-    await game.spawnItemUnique("trashbag3", place.get("slot_16"))
-    await game.spawnItemUnique("plasticbag", place.get("slot_17"))
-    await game.spawnItemUnique("trashbag4", place.get("slot_22"))
-    await game.spawnItemUnique("banana_bread", place.get("slot_26"))
-    await game.spawnItemUnique("trashbag5", place.get("slot_26"))
+
+    await game.spawnItemUnique("banana", blue_choice(slots))
+    await game.spawnItemUnique("oil_sunflower", blue_choice(slots))
+    for (let i = 0; i < 15; i++) {
+        await game.spawnItemUnique(blue_choice(food_list), blue_choice(slots))
+    }
+    for (let i = 0; i < 12; i++) {
+        await game.spawnItemUnique(blue_choice(trash_list), blue_choice(slots))
+    }
+    for (let i = 0; i < 8; i++) {
+        await game.spawnItemUnique(blue_choice(trashbags), blue_choice(slots))
+    }
 }
