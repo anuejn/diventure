e3895e129dfa935f2c364331f10e151b1563b8b5
Jaro Habiger <jarohabiger@googlemail.com>
2024-05-27 00:49:34 +0200

    spawn recipe for banana bread

--- game/places/book.ts
+++ game/places/book.ts
@@ -8,7 +8,7 @@ await game.spawnItemOnce("invitation", place.get("slot_2"));
 
 
 //to put papers back in order to read them
-const allowedItems = ["invitation", "invitation2", "shoppinglist", "cash", "meme"];
+const allowedItems = ["invitation", "invitation2", "shoppinglist", "cash", "meme", "recipe_bananabread"];
 place.getMany(/slot_\d/).map(slot => {
         slot.onOtherDrop(item => {
             console.log(item)
@@ -18,3 +18,7 @@ place.getMany(/slot_\d/).map(slot => {
             }
         })
 })
+
+if (game.state.wasDumpsterDiving) {
+    await game.spawnItemOnce("recipe_bananabread", place.get("slot_2"));
+}
