3d0fc871b03e82357f4ffdb3dce9ac73c7a7ae87
Jaro Habiger <jarohabiger@googlemail.com>
2024-06-08 22:46:31 +0200

    add hints re baking directly in the oven

--- game/places/kitchen.ts
+++ game/places/kitchen.ts
@@ -13,8 +13,20 @@ place.get('lower_closet').onClick(() => {
     game.getSound("closet_open").play();
     game.navigate('lower_closet')
 })
-place.get('dishes').onClick(() => {
+place.get('dishes').onClick(async () => {
     void game.getSound("plates").play()
+
+    const itemsInInventory = (await game.controls['inventory'].get('backpack_with_inventory').anchoredItemsRecursive()).map(x => x.itemName);
+    if (["flour", "sugar", "chocolate", "eggs", "butter"].some(x => itemsInInventory.includes(x))) {
+        const dialog = place.get("dialog_oven").dialog("right");
+        await dialog.sayOther("Hey!")
+        await dialog.sayOther("When you do this I get jealous!")
+        await dialog.sayOther("In this game you dont need to prepare dough")
+        await dialog.sayOther("Just put everything into me!")
+        await dialog.sayOther("I am a super-duper magical oven")
+        await sleep(1000)
+        await dialog.destroy()
+    }
 })
 
 if(game.state.wasDumpsterDiving){
--- game/places/upper_closet.ts
+++ game/places/upper_closet.ts
@@ -1,12 +1,22 @@
 place.getMany(/door\d/).forEach(door=>{
     door.onClick(() => {
-        game.getSound("closet_close").play();
+        void game.getSound("closet_close").play();
         game.navigate('kitchen')
     })
 })
 
-place.get('dishes').onClick(() => {
+place.get('dishes').onClick(async () => {
     void game.getSound("plates").play();
+    const itemsInInventory = (await game.controls['inventory'].get('backpack_with_inventory').anchoredItemsRecursive()).map(x => x.itemName);
+    if (["flour", "sugar", "chocolate", "eggs", "butter"].some(x => itemsInInventory.includes(x))) {
+        const dialog = place.get("dialog_dishes").dialog("right");
+        await dialog.sayOther("Psst...")
+        await dialog.sayOther("We are actually just part of the static drawing")
+        await dialog.sayOther("In this game you dont need to prepare dough")
+        await dialog.sayOther("Just put everything in the oven!")
+        await sleep(1000)
+        await dialog.destroy()
+    }
 })
 
 place.getMany(/slot_\d\d/).map(slot => {
