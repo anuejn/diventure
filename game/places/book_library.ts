bbc5f367522b001faf53a7ea041322eeaade9151
lisb-org <lisger.lg@gmail.com>
2024-06-13 12:03:52 +0200

    items and links

--- game/places/book_library.ts
+++ game/places/book_library.ts
@@ -6,3 +6,15 @@ place.get('cover').onClick(() => {
 place.get('robinfoods').onClick(() => {
     window.open("https://robin-foods.org", "_blank");
 })
+
+place.get('wastecooking').onClick(() => {
+    window.open("https://www.wastecooking.com/#home", "_blank");
+})
+
+place.get('quartalsbericht').onClick(() => {
+    window.open("https://edm.gv.at/lmw/#/berichte/oeffentlich", "_blank");
+})
+
+place.get('isnuguat').onClick(() => {
+    window.open("https://www.isnuguat.at", "_blank");
+})
--- game/places/tv.ts
+++ game/places/tv.ts
@@ -6,6 +6,10 @@ place.get('wastecooking').onClick(() => {
     window.open("https://www.youtube.com/@wastecooking", "_blank");
 })
 
+place.get('episode1').onClick(() => {
+    window.open("https://www.youtube.com/watch?v=aXByu2iraEA", "_blank");
+})
+
 place.get('semmerl').onClick(() => {
     window.open("https://www.youtube.com/watch?v=UlsIjPdAsOQ", "_blank");
 })
